c11a4cfe694ce6761cd77fee03522dec06007e35
Brendan Davis <brendanmd@gmail.com>
2017-06-27 13:42:11 -0700

    images are done

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var allArrays = []; //This is ALL our arrays
 var array1 = [];  //First set of images
 var newRay = []; //The current array
 var rightChoice = 0; //The correct choice for the game
+var cardBack = 'game-cards/bg.png'
 allArrays.push(array1);
 
 function imgArray(name, path) {//This is our constructor function
@@ -16,18 +17,18 @@ function imgArray(name, path) {//This is our constructor function
 new imgArray('t1', 'game-cards/travel/t1.png');
 new imgArray('t2', 'game-cards/travel/t2.png');
 new imgArray('t3', 'game-cards/travel/t3.png');
-new imgArray('t4', 'game-cards/travel/t1.png');
-new imgArray('t5', 'game-cards/travel/t1.png');
-new imgArray('t6', 'game-cards/travel/t1.png');
-new imgArray('t7', 'game-cards/travel/t1.png');
-new imgArray('t8', 'game-cards/travel/t1.png');
-new imgArray('t9', 'game-cards/travel/t1.png');
-new imgArray('t10', 'game-cards/travel/t1.png');
-new imgArray('t11', 'game-cards/travel/t1.png');
-new imgArray('t12', 'game-cards/travel/t1.png');
-new imgArray('t13', 'game-cards/travel/t1.png');
-new imgArray('t14', 'game-cards/travel/t1.png');
-new imgArray('t15', 'game-cards/travel/t1.png');
+new imgArray('t4', 'game-cards/travel/t4.png');
+new imgArray('t5', 'game-cards/travel/t5.png');
+new imgArray('t6', 'game-cards/travel/t6.png');
+new imgArray('t7', 'game-cards/travel/t7.png');
+new imgArray('t8', 'game-cards/travel/t8.png');
+new imgArray('t9', 'game-cards/travel/t9.png');
+new imgArray('t10', 'game-cards/travel/t10.png');
+new imgArray('t11', 'game-cards/travel/t11.png');
+new imgArray('t12', 'game-cards/travel/t12.png');
+new imgArray('t13', 'game-cards/travel/t13.png');
+new imgArray('t14', 'game-cards/travel/t14.png');
+new imgArray('t15', 'game-cards/travel/t15.png');
 // new imgArray('t16', 'game-cards/travel/t1.png');
 // new imgArray('t17', 'game-cards/travel/t1.png');
 // new imgArray('t1', 'game-cards/travel/t1.png');
@@ -67,7 +68,6 @@ function generateRay(array) { //Function to render 9 images
 };
 // Appending 9 images from (theArray)
 function appendAll(theArray){
-  // for (var i = 0 ; i < 3 ; i++){
     var point = 0;
     var trEl = document.createElement('tr');
     for (var j = 0; j < 9; j++){
@@ -80,19 +80,16 @@ function appendAll(theArray){
     table.appendChild(trEl);
   };
 
-  function appendBacks(theArray){
-    // for (var i = 0 ; i < 3 ; i++){
-      var point = 0;
-      var trEl = document.createElement('tr');
-      for (var j = 0; j < 9; j++){
-        var imgEl = document.createElement('img');
-        imgEl.src = newRay[point].path;
-        imgEl.name = 'newRay[point].name';
-        trEl.appendChild(imgEl);
-        point++;
-      }
-      table.appendChild(trEl);
-    };
+  function renderBacks(theArray){
+    var point = 0;
+    var trEl = document.createElement('tr');
+    for (var j = 0; j < 9; j++){
+      var imgEl = document.createElement('img');
+      imgEl.src = cardBack;
+      trEl.appendChild(imgEl);
+    }
+    table.appendChild(trEl);
+  };
 
 // Removes 1 image from the first 9 generated
 
@@ -124,6 +121,12 @@ function shuffle(array) { //Knuth shuffle - thanks Fisher-Yates!
   return array;
 }
 
+//wipes the existing images from the screen
+function clearImages() {
+  while (table.firstChild) {
+    table.removeChild(table.firstChild);
+  }
+}
+
 generateRay(array1); //Make that array
 appendAll(newRay); //Append the hell out of it
-
