ce4499524ca4afbbf1db2a58e45e469b3edd1d04
Brendan Davis <brendanmd@gmail.com>
2017-06-27 11:03:11 -0700

    replaces an image in the array, shuffles the images & appends the new set

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var table = document.getElementById('pinhere');
 var allArrays = []; //This is ALL our arrays
 var array1 = [];  //First set of images
 var newRay = []; //The current array
+var rightChoice = 0; //The correct choice for the game
 allArrays.push(array1);
 
 function imgArray(name, path) {//This is our constructor function
@@ -36,7 +37,8 @@ function randomImg(array) {
 
 function randomImgB(array) {
   var remove = Math.floor(Math.random() * (9));
-  return array[remove];
+  console.log(remove);
+  return remove;
 }
 // Generating 9 images from (array)
 function generateRay(array) { //Function to render 9 images
@@ -69,11 +71,32 @@ function appendAll(theArray){
     table.appendChild(trEl);
   };
 // Removes 1 image from the first 9
-// function replaceImage(array) {
-//   console.log(newRay);
-//   newRay.splice(randomImgB(array, 1));
-//   console.log(newRay);
-//   var rightChoice = array[9];
-// }
+function replaceImage(array) {
+  console.log(array);
+  newRay.splice(randomImgB(array), 1);
+  console.log(array);
+  var rightChoice = array[9];
+  shuffle(array);
+  appendAll(newRay);
+}
+
+function shuffle(array) { //Knuth shuffle - thanks Fisher-Yates!
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
 generateRay(array1);
 appendAll(newRay);
