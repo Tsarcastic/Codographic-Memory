579dee68801850c46d650983a51e7a46db2eea2c
Brendan Davis <brendanmd@gmail.com>
2017-06-28 17:18:39 -0700

    local storage on page 1 is working

--- page1JS.js
+++ page1JS.js
@@ -7,19 +7,27 @@ var theForm = document.getElementById('theForm');
 function User(name, age) {
   this.name = name;
   this.age = age;
+  this.score = 0;
   data.push(this)
+  }
+
+  function pushThing(otherThing) {
+    localStorage.clear();
+    var otherThingJSON = JSON.stringify(otherThing);
+    localStorage.things = otherThingJSON;
   }
 
 function readData(event) {
   event.preventDefault();
-  var name = event.target.name.value;
-  var age = event.target.age.value;
-  console.log();
+  var name = theForm.name.value;
+  var age = theForm.age.value;
   var newUser = new User(name, age);
-  console.log('readData is working properly');
+  pushThing(newUser);
   console.log(newUser);
   return newUser;
 }
+
+
 
 document.getElementById('theForm').addEventListener('submit', readData)
 document.getElementById('test').addEventListener('click', readData);
