a0359f6a01829d216025ed8b72d3a6cf2ac94bfc
Brendan Davis <brendanmd@gmail.com>
2017-06-29 11:25:43 -0700

    localStorage is moving forward

--- page1JS.js
+++ page1JS.js
@@ -6,7 +6,6 @@ var theForm = document.getElementById('theForm');
 
 function User(name, age) {
   this.name = name;
-  this.age = age;
   this.score = 0;
   data.push(this);
 }
@@ -20,14 +19,13 @@ function pushThing(otherThing) {
 function readData(event) {
   event.preventDefault();
   var name = theForm.name.value;
-  var age = theForm.age.value;
-  var newUser = new User(name, age);
+  var newUser = new User(name);
   pushThing(newUser);
   console.log(newUser);
-  return newUser;
+  window.location.href = "page2.html"
 }
 
 
 
 document.getElementById('theForm').addEventListener('submit', readData);
-document.getElementById('test').addEventListener('click', readData);
+document.getElementById('play').addEventListener('click', readData);
